208626d542469174fa5a0417c29285f4697945ec
Rudolf Engelke <rue2006@qatar-med.cornell.edu>
2020-03-02 14:12:43 +0300

    improve read_olink and prepare_olink functions

--- R/prepare_olink.R
+++ R/prepare_olink.R
@@ -1,52 +1,107 @@
+
+
+# file <- "C:/Users/rue2006/Documents/R_wd/mazloum_mouse/data/20200203_WCQO-20-001B/20200203_WCQO-20-001B.csv"
+# object <- read_olink(file)
+#
+# filter_sample_type = c("Control", "Sample")
+# filter_feature_type = c("Control", "Protein")
+# filter_sample_quality = c("Warning", "Pass")
+# filter_feature_quality = 1.0
+# rm_features_below_LOD = TRUE
+#
+# rm_complete_feature_nondetects = TRUE
+# rm_complete_sample_nondetects = TRUE
+# rm_single_value_svars = FALSE
+# rm_single_value_fvars = FALSE
+# rm_na_svars = TRUE
+# rm_na_fvars = TRUE
+# log10transform = FALSE
+
+
+#' Prepare Olink
+#'
+#' @param object SummarizedExperiment
+#' @param filter_sample_type string vector: sample  types to be filtered for. Subset of c('Sample', 'Control').
+#' @param filter_feature_type string vector: feature types to be filtered for. Subset of c('Protein', 'Control')
+#' @param filter_sample_quality string vector: sample  qualities to be filtered for. Subset of c('Pass', 'Warning')
+#' @param filter_feature_quality number: fraction 0-1 indicating missing data frequency cutoff
+#' @param rm_features_below_lod_for_some_sample logical: wether to remove values below limit of detection (LOD)
+#' @param rm_complete_feature_nondetects logical: wether to remove complete feature nondetects
+#' @param rm_complete_sample_nondetects logical: wether to remove complete sample nondetects
+#' @param rm_single_value_svars logical:  whether to remove single value svars
+#' @param rm_single_value_fvars logical:  whether to remove single value fvars
+#'
+#' @return
+#' @export SummarizedExperiment
+#'
+#' @examples prepare_olink(object)
 prepare_olink <- function (object,
-                           filter_sample_type = c("Control", "Sample"),
-                           filter_sample_quality = c("Warning", "Pass"),
-                           filter_feature_type = c("Control", "Protein"),
+                           filter_sample_type = c("Sample", "Control"),
+                           filter_feature_type = c("Protein", "Control"),
+                           filter_sample_quality = c("Pass", "Warning"),
                            filter_feature_quality = 1.0,
+                           rm_features_below_lod_for_some_sample = TRUE,
                            rm_complete_feature_nondetects = TRUE,
                            rm_complete_sample_nondetects = TRUE,
                            rm_single_value_svars = FALSE,
-                           rm_single_value_fvars = FALSE,
-                           rm_na_svars = TRUE,
-                           rm_na_fvars = TRUE,
-                           log10transform = FALSE)
+                           rm_single_value_fvars = FALSE) {
 
-{
-    SampleType <- RowCheck <- Type <- ColCheck <- NULL
+
     assertive.types::assert_is_character(c(filter_sample_type,
                                            filter_sample_quality, filter_feature_type))
-    assertive.types::assert_is_a_number(filter_feature_quality)
-    assertive.types::assert_is_logical(c(rm_complete_feature_nondetects,
-                                         rm_complete_sample_nondetects, rm_single_value_svars,
-                                         rm_single_value_fvars, rm_na_svars, rm_na_fvars,
-                                         log10transform))
+    filter_feature_quality %>% assertive.types::assert_is_a_number() %>%
+        assertive.numbers::assert_all_are_greater_than(0) %>%
+        assertive.numbers::assert_all_are_less_than_or_equal_to(1)
+    assertive.types::assert_is_logical(c(rm_features_below_lod_for_some_sample,
+                                         rm_complete_feature_nondetects,
+                                         rm_complete_sample_nondetects,
+                                         rm_single_value_svars,
+                                         rm_single_value_fvars))
+
+    filter_sample_type %<>% match.arg(c("Sample", "Control"), several.ok = TRUE)
+    filter_feature_type %<>% match.arg(c("Protein", "Control"), several.ok = TRUE)
+    filter_sample_quality %<>% match.arg(c("Pass", "Warning"), several.ok = TRUE)
+
+
 
     # filter_sample_type
-    if ("Assay" %in% autonomics.import::svars(object)) {
-        sdata(object)$SampleType <- ifelse(
-            sdata(object)$Assay %>%
-                stringr::str_detect("^Neg Ctrl |^Ext Ctrl |^IPC "), "Control", "Sample")
-        idx <- sdata(object)$SampleType %in% filter_sample_type
+    if ("sample_id" %in% autonomics.import::svars(object)) {
+        autonomics.import::sdata(object)$sample_type <- ifelse(
+            autonomics.import::sdata(object)$sample_id %>%
+                stringr::str_detect("^Neg Ctrl |^Ext Ctrl |^IPC "),
+            "Control", "Sample")
+        idx <- sdata(object)$sample_type %in% filter_sample_type
         idx <- idx & !is.na(idx)
         object %<>% magrittr::extract(, idx)
     }
-    if ("QC Warning" %in% autonomics.import::svars(object)) {
-        idx <- sdata(object)$`QC Warning` %in% filter_sample_quality
+
+    #filter_feature_type
+    if ("Assay" %in% autonomics.import::fvars(object)) {
+        autonomics.import::fdata(object)$feature_type <- ifelse(
+            autonomics.import::fdata(object)$Assay %>%
+                stringr::str_detect("^Inc Ctrl |^Det Ctrl |^Ext Ctrl "),
+            "Control", "Protein")
+        idx <- fdata(object)$feature_type %in% filter_feature_type
+        idx <- idx & !is.na(idx)
+        object %<>% magrittr::extract(idx, )
+        autonomics.import::fdata(object) %<>% droplevels()
+    }
+
+    if ("QC_Warning" %in% autonomics.import::svars(object)) {
+        idx <- autonomics.import::sdata(object)$QC_Warning %in% filter_sample_quality
         idx <- idx & !is.na(idx)
         object %<>% magrittr::extract(, idx)
     }
-    if ("OlinkID" %in% autonomics.import::fvars(object)) {
-        fdata(object)$Type <- ifelse(
-            fdata(object)$OlinkID %>% is.na(), "Control", "Protein")
-        idx <- fdata(object)$Type %in% filter_feature_type
-        idx <- idx & !is.na(idx)
+
+    if ("Missing_Data_freq" %in% autonomics.import::fvars(object)) {
+        idx <- autonomics.import::fdata(object)$Missing_Data_freq <= filter_feature_quality
         object %<>% magrittr::extract(idx, )
-        fdata(object) %<>% droplevels()
+        autonomics.import::fdata(object) %<>% droplevels()
     }
-    if ("Missing Data freq." %in% autonomics.import::fvars(object)) {
-        idx <- fdata(object)$`Missing Data freq.` <= filter_feature_quality
-        object %<>% magrittr::extract(idx, )
-        fdata(object) %<>% droplevels()
+
+    if (rm_features_below_lod_for_some_sample) {
+        exprs(object) %<>% sweep(., 2, fdata(object)$LOD, `-`) %>%
+            replace(. <= 0, NA)
     }
 
     if (rm_complete_feature_nondetects) {
@@ -64,13 +119,6 @@ prepare_olink <- function (object,
         autonomics.import::sdata(object) %<>% autonomics.support::rm_single_value_columns()
     if (rm_single_value_fvars)
         autonomics.import::fdata(object) %<>% autonomics.support::rm_single_value_columns()
-    if (rm_na_fvars)
-        autonomics.import::fdata(object) %<>% autonomics.support::rm_na_columns()
-    if (rm_na_svars)
-        autonomics.import::sdata(object) %<>% autonomics.support::rm_na_columns()
-
-    if (log10transform)
-        exprs(object) %<>% log10()
 
     object
 }
--- R/read_olink.R
+++ R/read_olink.R
@@ -1,93 +1,173 @@
-read_olink <- function (file, fid_var = "OlinkID", fname_var = "Assay")
-{
+
+# # # Does not work with read_omics as the underlying extract_rectangle because:
+# # data.table::fread(file,
+# #                   na.strings = "",
+# #                   header     = FALSE,
+# #                   integer64  = 'numeric', fill=TRUE, sep = ",")
+#
+# file <- "C:/Users/rue2006/Documents/R_wd/mazloum_mouse/data/20200203_WCQO-20-001B/20200203_WCQO-20-001B.csv"
+
+#' Read Olink
+#'
+#' Read data from Olink csv file
+#'
+#' @param file string: path to Olink NPX data csv file
+#'
+#' @return SummarizedExperiment
+#' @seealso prepare_olink
+#' @importFrom magrittr %>% %<>%
+#' @export
+#'
+#' @examples read_olink('extdata/olink_mouse_expl.csv')
+read_olink <- function (file) {
+
+    # Assert
     assertive.files::assert_all_are_existing_files(file)
     assertive.types::assert_is_a_string(fid_var)
     assertive.types::assert_is_a_string(fname_var)
-    content <- readLines(file)
-    n_row <- length(content)
-    n_col <- utils::count.fields(file, quote = "", sep = ",") %>%
-        max()
-    f_row <- 1 + which(stringi::stri_detect_fixed(content, "NPX data"))
-    s_row_h <- content %>% magrittr::extract(f_row:length(.)) %>%
-        purrr::detect_index(function(y) stringi::stri_detect_regex(y,
-                                                                   "QC", negate = FALSE)) %>% magrittr::add(f_row - 1)
-    s_row <- content %>% magrittr::extract(f_row:length(.)) %>%
-        purrr::detect_index(function(y) stringi::stri_detect_regex(y,
-                                                                   "^\\,", negate = FALSE)) %>% magrittr::add(f_row - 1)
-    f_col <- 1
-    fid_cols <- (f_col + 1):(n_col - 4)
-    fid_rows <- content %>% magrittr::extract(f_row:(s_row -
-                                                         1)) %>% stringi::stri_extract_first_words() %>% magrittr::equals(fid_var) %>%
-        which() %>% magrittr::add(f_row - 1)
-    sid_rows <- (1 + s_row):(n_row - 3)
-    sid_cols <- 1
-    # object <- file %>% autonomics.import:::read_omics(
-    #     fid_rows = fid_rows,
-    #     fid_cols = fid_cols,
-    #     sid_rows = sid_rows,
-    #     sid_cols = sid_cols,
-    #     expr_rows = (s_row + 1):(n_row - 3),
-    #     expr_cols = (f_col + 1):(n_col - 4),
-    #     fvar_rows = c(f_row:(s_row - 1), (n_row - 1):n_row),
-    #     fvar_cols = f_col,
-    #     fdata_rows = c(f_row:(s_row - 1), (n_row - 1):n_row),
-    #     fdata_cols = (f_col + 1):(n_col - 4),
-    #     svar_rows = s_row_h,
-    #     svar_cols = (n_col - 3):n_col,
-    #     sdata_rows = (s_row + 1):(n_row - 3),
-    #     sdata_cols = (n_col - 3):n_col,
-    #     transpose = T, verbose = F)
 
-    object_raw <- read.csv(file, header = FALSE, stringsAsFactors = FALSE,
+    fid_var <- "OlinkID"
+    fname_var <- "Assay"
+
+
+    # Initial Read
+    object_raw <- read.csv(file,
+                           header = FALSE,
+                           stringsAsFactors = FALSE,
+                           blank.lines.skip = FALSE,
                            na.strings = c("NA", "NaN", ""))
 
-    expr_rows <- (s_row + 1):(n_row - 3)
-    expr_cols <- (f_col + 1):(n_col - 4)
+
+    fid_rows = 6   #according to fid_var
+    fid_cols = 2:97
+
+    sid_rows = 8:103
+    sid_cols = 1
+
+    expr_rows	= 8:103
+    expr_cols	= 2:97
+
+    fvar_rows	= c(3:6, 105:106)
+    fvar_cols	= 1
+
+    svar_rows	= 4
+    svar_cols	= 98:101
+
+    fdata_rows = c(3:6, 105:106)
+    fdata_cols = 2:97
+
+    sdata_rows = 8:103
+    sdata_cols = 98:101
+
+
+    fname_rows = 4  #according to fname_var
+    fname_cols = 2:97
+
+
+
+    # Extract exprs
+
+    fids1 <- object_raw[fid_rows, fid_cols] %>% as.character()
+
+    sids1 <- object_raw[sid_rows, sid_cols] %>% as.character()
 
     exprs1 <- object_raw[expr_rows, expr_cols] %>%
-        `colnames<-`(object_raw[fid_rows, fid_cols] %>% make.names(unique = TRUE)) %>%
-        `rownames<-`(object_raw[sid_rows, sid_cols]) %>%
-        data.matrix()
-
-    fvar_rows <- c(f_row:(s_row - 1), (n_row - 1):n_row)
-    fvar_cols <- f_col
-    fdata_rows <- c(f_row:(s_row - 1), (n_row - 1):n_row)
-    fdata_cols <- (f_col + 1):(n_col - 4)
+        data.matrix() %>% t() %>%
+        `colnames<-`(fids1 %>% make.names(unique = TRUE)) %>%
+        `rownames<-`(sids1)
 
 
-    fdata1 <- object_raw[fdata_rows, fdata_cols] %>% t() %>%
-        `rownames<-`(1:nrow(.)) %>%
-        `colnames<-`(object_raw[fvar_rows, fvar_cols]) %>%
-        as.data.frame(stringsAsFactors = FALSE)
-    fdata1$LOD %<>% as.numeric()
-    fdata1$`Missing Data freq.` %<>% stringr::str_remove("%") %>%
-        as.numeric() %>%
-        magrittr::divide_by(100)
-    rownames(fdata1) <- fdata1$OlinkID %>% make.names(unique = TRUE)
+    # Extract feature annotations
+    #    Leave rownames(fdata1) empty: fids1 may contain non-valid values
+    #    This happens in MaxQuant files, which sometimes contain missing rows (I think after opening in excell)
+    fdata1 <- data.frame(feature_id = fids1, stringsAsFactors = FALSE)
+    fdata_available <- !is.null(fvar_rows) & !is.null(fvar_cols)
+    if (fdata_available){
+        fvars1 <- object_raw[fvar_rows, fvar_cols] %>%
+            stringr::str_replace_all(" ", "_") %>%
+            stringr::str_replace_all("\\.", "")
+        fdata1 %<>% cbind(t(object_raw[fdata_rows, fdata_cols]), stringsAsFactors = FALSE) %>%
+            `colnames<-`(c("feature_id", fvars1)) %>%
+            data.frame(stringsAsFactors = FALSE, check.names = FALSE)
 
-    svar_rows <- s_row_h
-    svar_cols <- c(sid_cols, (n_col - 3):n_col)
-    sdata_rows <- (s_row + 1):(n_row - 3)
-    sdata_cols <- c(sid_cols, (n_col - 3):n_col)
+        if ("LOD" %in% fvars1) {
+            fdata1$LOD %<>% as.numeric()
+        }
+        if ("Missing_Data_freq" %in% fvars1) {
+            fdata1$Missing_Data_freq %<>%
+                stringr::str_replace("\\%", "") %>%
+                as.numeric() %>% `/`(100)
+        }
+    }
 
-    sdata1 <- object_raw[sdata_rows, sdata_cols] %>%
-        `colnames<-`(object_raw[svar_rows, svar_cols]) %>%
-        `rownames<-`(object_raw[sid_rows, sid_cols])
 
-    object <- SummarizedExperiment::SummarizedExperiment(assays = list(exprs = exprs1),
-                                                         rowData = fdata1,
-                                                         colData = sdata1)
 
-    # sdata(object) %>% (function(y) {
-    #     y$subgroup <- y[[subgroup_var]]
-    #     y %>% autonomics.support::pull_columns(c("sample_id",
-    #         "subgroup"))
-    # })
 
-    fdata(object) %>% (function(y) {
+    # Extract sample annotations
+    #    Leave rownames(sdata1) empty: sids may contain non-valid values
+    #    This happens in SOMA files, where CLIENT_IDENTIFIER is not unique for calibrator and buffer samples
+    sdata1 <- data.frame(sample_id = sids1, stringsAsFactors = FALSE)
+    sdata_available <- !is.null(svar_rows) & !is.null(svar_cols)
+    if (sdata_available){
+        svars1 <- object_raw[svar_rows, svar_cols] %>%
+            stringr::str_replace_all(" ", "_")
+        svars1[grepl(svars1, pattern = "QC_Deviation")] %<>%
+            paste0(., c("_Inc_Ctrl", "_Det_Ctrl"))
+        sdata1 %<>% cbind(object_raw[sdata_rows, sdata_cols], stringsAsFactors = FALSE) %>%
+            `colnames<-`(c("sample_id", svars1)) %>%
+            data.frame(stringsAsFactors = FALSE, check.names = FALSE)
+
+        if ("QC_Deviation_from_median_Inc_Ctrl" %in% svars1) {
+            sdata1$QC_Deviation_from_median_Inc_Ctrl %<>% as.numeric()
+        }
+        if ("QC_Deviation_from_median_Inc_Ctrl" %in% svars1) {
+            sdata1$QC_Deviation_from_median_Det_Ctrl %<>% as.numeric()
+        }
+    }
+
+
+    # Rm samples with missing ids
+    idx <- !is.na(sids1)
+    if (any(!idx)){
+        sids1  %<>% magrittr::extract(idx)
+        sdata1 %<>% magrittr::extract(idx,)
+        exprs1 %<>% magrittr::extract(, idx)
+    }
+
+
+    # Name features and samples
+    fids1 %<>% make.names(unique = TRUE)
+    #fids1 %<>% autonomics.support::uniquify('make.unique')  # will not remove first NA
+    sids1 %<>% autonomics.support::uniquify('make.unique')
+    rownames(exprs1) <- rownames(fdata1) <- fids1
+    colnames(exprs1) <- rownames(sdata1) <- sids1
+
+
+
+    object <- SummarizedExperiment::SummarizedExperiment(
+        assays = list(exprs = exprs1))
+    autonomics.import::fdata(object) <- fdata1     # fdata rownames are lost!
+    autonomics.import::sdata(object) <- sdata1
+    autonomics.import::analysis(object) <- list(
+        nfeatures = c(all = nrow(exprs1)),
+        nsamples  = c(all = ncol(exprs1)))
+
+
+    # sdata
+    autonomics::sdata(object) %<>% autonomics.support::pull_columns('sample_id')
+
+    # fdata
+    assertive.sets::assert_is_subset(fname_var, autonomics::fvars(object))
+    assertive.sets::assert_is_subset("Uniprot_ID", autonomics::fvars(object))
+    autonomics.import::fdata(object) %<>% (function (y) {
         y$feature_name <- y[[fname_var]]
-        y %>% autonomics.support::pull_columns(c("feature_id",
-                                                 "feature_name"))
+        y$feature_uniprot <- y[["Uniprot_ID"]]
+        y %>% autonomics.support::pull_columns(c('feature_id', 'feature_name',
+                                                 'feature_uniprot'))
     })
+
+
+    # Return
     object
+
 }
