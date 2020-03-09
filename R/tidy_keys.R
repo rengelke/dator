40b0551072f2740824d437da1ddce43132561b47
Rudolf Engelke <rue2006@qatar-med.cornell.edu>
2020-03-09 15:35:24 +0300

    add na.rm option

--- R/tidy_keys.R
+++ R/tidy_keys.R
@@ -3,6 +3,7 @@
 #' Clean keys
 #'
 #' @param x vector containing database identifiers
+#' @param na.rm logical, remove NA values
 #'
 #' @return
 #' @export
@@ -10,16 +11,23 @@
 #' @importFrom stringr str_split
 #'
 #' @examples tidy_keys(c("68195; 100037283" "224903", "NaN", NA, "14660", "", "67532, 100034361"))
-tidy_keys <- function (x) {
+tidy_keys <- function (x, na.rm = FALSE) {
 
-    x %>%
+    x %<>%
         as.character() %>%
-        .[.!=""] %>%
-        .[.!="NA"] %>%
-        .[.!="NaN"] %>%
         stringr::str_split(., ";", simplify = TRUE) %>% .[, 1] %>%
         stringr::str_split(., ",", simplify = TRUE) %>% .[, 1]
 
+    x %<>%
+        replace(.=="", NA) %>%
+        replace(.=="NA", NA) %>%
+        replace(.=="NaN", NA)
+
+    if (na.rm) {
+        x[!is.na(x)]
+    } else {
+        x
+    }
 
 }
 
